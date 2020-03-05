5b74a21bc9d48182f0606d1ae970c1e6523e63b9
AnqiPang <anqi.pang@mail.utoronto.ca>
2020-03-04 21:49:43 -0500

    update ami id

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 #ami_id = 'ami-a076eec9'
 #ami_id = 'ami-080bb209625b6f74f'
-ami_id = 'ami-0c6e9127d6c9a58dc'
+ami_id = 'ami-0e44f9146b33e72d9'
 zone = 'us-east-1d'
 security_group= 'sg-0011e3d3b7d4a13e9'
 keyname = 'ece1779a2'
